11a72d905ea5582f25ed680166f4ec0310ece4fe
Noah Boegli <39568628+noahboegli@users.noreply.github.com>
2020-11-13 08:28:10 +0100

    Create iscovid19.js

---
+++ iscovid19.js
@@ -0,0 +1,21 @@
+export class IsCovid19{
+    static aboutToDestroyTheWorld(){
+        return 0;
+    }
+    static aHoax(){
+        return 0;
+    }
+    static stagedByBillGates(){
+        return 0;
+    }
+    static aThreatToHumanity(){
+        return 0.75;
+    }
+    static theRealNameOfTheVirus(extended = false){
+        if(extended){
+            return "No, the SARS-CoV-2 coronavirus cause the CoVID-19 disease."
+        }else{
+            return false;
+        }
+    }
+}
